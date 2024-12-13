81fec24aef092ecb55110f0faee787cc7dd1376b
João Italo Deodato <166622223+Happjoi@users.noreply.github.com>
2024-12-13 13:05:48 -0300

    Create DESCONTOS (TRIGGERS).sql

---
+++ SCRIPTS/DESCONTOS (TRIGGERS).sql
@@ -0,0 +1,39 @@
+UPDATE `salaosenac`.`tbl_itensServicos`
+SET desconto = desconto + 10 -- Aumenta o desconto em 10%
+WHERE tbl_agendamento_idtbl_agendamento = 2 AND tbl_servico_idtbl_servico = 2;
+
+UPDATE `salaosenac`.`tbl_itensServicos`
+SET desconto = desconto + 20 -- Aumenta o desconto em 20%
+WHERE tbl_agendamento_idtbl_agendamento = 4 AND tbl_servico_idtbl_servico = 4;
+
+UPDATE `salaosenac`.`tbl_itensServicos`
+SET desconto = desconto + 7 -- Aumenta o desconto em 7%
+WHERE tbl_agendamento_idtbl_agendamento = 7 AND tbl_servico_idtbl_servico = 7;
+
+UPDATE `salaosenac`.`tbl_itensServicos`
+SET desconto = desconto + 5 -- Aumenta o desconto em 5%
+WHERE tbl_agendamento_idtbl_agendamento = 8 AND tbl_servico_idtbl_servico = 8;
+
+UPDATE `salaosenac`.`tbl_itensServicos`
+SET desconto = desconto + 3 -- Aumenta o desconto em 3%
+WHERE tbl_agendamento_idtbl_agendamento = 10 AND tbl_servico_idtbl_servico = 10;
+
+UPDATE `salaosenac`.`tbl_itensServicos`
+SET desconto = desconto + 8 -- Aumenta o desconto em 8%
+WHERE tbl_agendamento_idtbl_agendamento = 11 AND tbl_servico_idtbl_servico = 11;
+
+UPDATE `salaosenac`.`tbl_itensServicos`
+SET desconto = desconto + 6 -- Aumenta o desconto em 6%
+WHERE tbl_agendamento_idtbl_agendamento = 12 AND tbl_servico_idtbl_servico = 12;
+
+UPDATE `salaosenac`.`tbl_itensServicos`
+SET desconto = desconto + 18 -- Aumenta o desconto em 18%
+WHERE tbl_agendamento_idtbl_agendamento = 13 AND tbl_servico_idtbl_servico = 13;
+
+UPDATE `salaosenac`.`tbl_itensServicos`
+SET desconto = desconto + 20 -- Aumenta o desconto em 20%
+WHERE tbl_agendamento_idtbl_agendamento = 14 AND tbl_servico_idtbl_servico = 14;
+
+UPDATE `salaosenac`.`tbl_itensServicos`
+SET desconto = desconto + 9 -- Aumenta o desconto em 9%
+WHERE tbl_agendamento_idtbl_agendamento = 15 AND tbl_servico_idtbl_servico = 15;
